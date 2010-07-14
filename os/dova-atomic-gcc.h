4d5c8935d2ee614773b648d8902ac354e1b038da
Jürg Billeter <j@bitron.ch>
2010-07-14 14:27:22 +0200

    Move gcc-specific atomic support to separate header file

--- os/dova-atomic-gcc.h
+++ os/dova-atomic-gcc.h
@@ -1,4 +1,4 @@
-/* dova-os.h
+/* dova-atomic-gcc.h
  *
  * Copyright (C) 2010  Jürg Billeter
  *
@@ -20,34 +20,25 @@
  * 	Jürg Billeter <j@bitron.ch>
  */
 
-#ifndef __DOVA_OS_H__
-#define __DOVA_OS_H_
+#ifndef __DOVA_ATOMIC_GCC_H__
+#define __DOVA_ATOMIC_GCC_H_
 
-#include <arpa/inet.h>
-#include <assert.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <math.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sched.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/epoll.h>
-#include <sys/mman.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/timerfd.h>
-#include <sys/types.h>
-#include <sys/un.h>
-#include <time.h>
-#include <ucontext.h>
-#include <unistd.h>
+static inline void atomic_int32_store (volatile int32_t *atomic, int32_t desired) {
+	*atomic = desired;
+	__sync_synchronize ();
+}
 
-#include "dova-atomic-gcc.h"
-#include "dova-threads-pthread.h"
+static inline int32_t atomic_int32_load (volatile int32_t *atomic) {
+	__sync_synchronize ();
+	return *atomic;
+}
+
+static inline int32_t atomic_int32_fetch_add (volatile int32_t *atomic, int32_t operand) {
+	return __sync_fetch_and_add (atomic, operand);
+}
+
+static inline int32_t atomic_int32_fetch_sub (volatile int32_t *atomic, int32_t operand) {
+	return __sync_fetch_and_sub (atomic, operand);
+}
 
 #endif
