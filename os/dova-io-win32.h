411ce4e8c76f8f0ad785cb991b48cb4b663a2547
Jürg Billeter <j@bitron.ch>
2010-09-01 19:18:10 +0200

    os: Add initial socket support for Windows

--- os/dova-io-win32.h
+++ os/dova-io-win32.h
@@ -24,10 +24,68 @@
 #define __DOVA_IO_WIN32_H_
 
 #include <io.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define SOCK_CLOEXEC 02000000
+#define SOCK_NONBLOCK 04000
 
 static int _dova_open (const char *pathname, int flags, unsigned int mode) {
 	return open (pathname, flags | O_BINARY, mode);
 }
 #define open _dova_open
+
+static inline int _dova_socket (int domain, int type, int protocol) {
+	SOCKET s;
+	WSADATA wsadata;
+
+	if (WSAStartup (MAKEWORD (2, 2), &wsadata)) {
+		return -1;
+	}
+
+	s = socket (domain, type, protocol);
+	if (s == INVALID_SOCKET) {
+		return -1;
+	}
+
+	/* set socket to non-blocking */
+	if (type & SOCK_NONBLOCK) {
+		unsigned long arg = 1;
+		ioctlsocket (s, FIONBIO, &arg);
+	}
+
+	return _open_osfhandle ((intptr_t) s, 0);
+}
+#define socket _dova_socket
+
+static inline int _dova_bind (int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+	if (bind (_get_osfhandle (sockfd), addr, addrlen) == SOCKET_ERROR) {
+		return -1;
+	}
+
+	return 0;
+}
+#define bind _dova_bind
+
+static inline int _dova_connect (int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+	if (connect (_get_osfhandle (sockfd), addr, addrlen) == SOCKET_ERROR) {
+		return -1;
+	}
+
+	return 0;
+}
+#define connect _dova_connect
+
+static inline int _dova_accept (int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+	SOCKET s;
+
+	s = accept (_get_osfhandle (sockfd), addr, addrlen);
+	if (s == INVALID_SOCKET) {
+		return -1;
+	}
+
+	return _open_osfhandle ((intptr_t) s, 0);
+}
+#define accept _dova_accept
 
 #endif
