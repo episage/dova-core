39f6e5a8076bfafc80ffe9b3902fdb80548eb718
Jürg Billeter <j@bitron.ch>
2010-09-02 10:51:33 +0200

    os: Add initial epoll support for Windows

--- os/dova-io-win32.h
+++ os/dova-io-win32.h
@@ -30,6 +30,43 @@
 #define SOCK_CLOEXEC 02000000
 #define SOCK_NONBLOCK 04000
 
+struct _dova_epoll {
+	int n_fds;
+	struct _dova_epoll_fd *fds;
+};
+
+typedef struct _dova_epoll *epoll_t;
+
+#define EPOLL_CTL_ADD 1
+#define EPOLL_CTL_DEL 2
+#define EPOLL_CTL_MOD 3
+
+#define EPOLLIN (FD_READ | FD_ACCEPT | FD_CLOSE)
+#define EPOLLOUT (FD_WRITE | FD_CONNECT | FD_CLOSE)
+
+#define EPOLL_CLOEXEC 02000000
+#define EPOLL_NONBLOCK 04000
+
+typedef union epoll_data {
+	void *ptr;
+	int fd;
+	uint32_t u332;
+	uint64_t u64;
+} epoll_data_t;
+
+struct epoll_event {
+	uint32_t events;
+	epoll_data_t data;
+};
+
+struct _dova_epoll_fd {
+	struct _dova_epoll_fd *next;
+	struct _dova_epoll_fd *prev;
+	int fd;
+	struct epoll_event event;
+	WSAEVENT event_handle;
+};
+
 static int _dova_open (const char *pathname, int flags, unsigned int mode) {
 	return open (pathname, flags | O_BINARY, mode);
 }
@@ -87,5 +124,127 @@ static inline int _dova_accept (int sockfd, struct sockaddr *addr, socklen_t *ad
 	return _open_osfhandle ((intptr_t) s, 0);
 }
 #define accept _dova_accept
+
+static inline epoll_t epoll_create1 (int flags) {
+	epoll_t epfd = calloc (1, sizeof (struct _dova_epoll));
+	return epfd;
+}
+
+static inline int epoll_ctl (epoll_t epfd, int op, int fd, struct epoll_event *event) {
+	struct _dova_epoll_fd *epoll_fd;
+
+	for (epoll_fd = epfd->fds; epoll_fd; epoll_fd = epoll_fd->next) {
+		if (epoll_fd->fd == fd) {
+			switch (op) {
+			case EPOLL_CTL_ADD:
+				errno = EEXIST;
+				return -1;
+			case EPOLL_CTL_MOD:
+				epoll_fd->event = *event;
+				return 0;
+			case EPOLL_CTL_DEL:
+				if (epoll_fd->event_handle) {
+					WSACloseEvent (epoll_fd->event_handle);
+				}
+
+				if (epoll_fd->next == epoll_fd) {
+					/* only fd */
+					epfd->fds = NULL;
+				} else {
+					epoll_fd->prev->next = epoll_fd->next;
+					epoll_fd->next->prev = epoll_fd->prev;
+					if (epoll_fd == epfd->fds) {
+						epfd->fds = epoll_fd->next;
+					}
+				}
+				free (epoll_fd);
+				return 0;
+			}
+		}
+		if (epoll_fd->next == epfd->fds) {
+			break;
+		}
+	}
+
+	if (op == EPOLL_CTL_ADD) {
+		epoll_fd = calloc (1, sizeof (struct _dova_epoll_fd));
+		epoll_fd->fd = fd;
+		epoll_fd->event = *event;
+
+		if (GetFileType ((HANDLE) _get_osfhandle (fd)) == FILE_TYPE_PIPE) {
+			long events;
+			epoll_fd->event_handle = WSACreateEvent ();
+			WSAEventSelect (_get_osfhandle (fd), epoll_fd->event_handle, event->events);
+		}
+
+		if (epfd->fds == NULL) {
+			epoll_fd->next = epoll_fd;
+			epoll_fd->prev = epoll_fd;
+			epfd->fds = epoll_fd;
+		} else {
+			epoll_fd->next = epfd->fds;
+			epoll_fd->prev = epfd->fds->prev;
+			epoll_fd->prev->next = epoll_fd;
+			epoll_fd->next->prev = epoll_fd;
+		}
+
+		return 0;
+	} else {
+		errno = ENOENT;
+		return -1;
+	}
+}
+
+static inline int epoll_wait (epoll_t epfd, struct epoll_event *events, int maxevents, int timeout) {
+	HANDLE *handles;
+	int nhandles, nevents;
+	DWORD ret;
+	struct _dova_epoll_fd *epoll_fd;
+
+	handles = calloc (epfd->n_fds, sizeof (HANDLE));
+
+	nhandles = 0;
+
+	for (epoll_fd = epfd->fds; epoll_fd; epoll_fd = epoll_fd->next) {
+		if (epoll_fd->event_handle) {
+			handles[nhandles] = epoll_fd->event_handle;
+		} else {
+			handles[nhandles] = (HANDLE) _get_osfhandle (epoll_fd->fd);
+		}
+		nhandles++;
+
+		if (epoll_fd->next == epfd->fds) {
+			break;
+		}
+	}
+
+	ret = WaitForMultipleObjects (nhandles, handles, FALSE, timeout);
+
+	if (ret == WAIT_FAILED) {
+		/* TODO: handle all error cases */
+	}
+
+	nhandles = 0;
+	nevents = 0;
+
+	for (epoll_fd = epfd->fds; epoll_fd; epoll_fd = epoll_fd->next) {
+		if (ret == WAIT_OBJECT_0 + nhandles) {
+			events[nevents] = epoll_fd->event;
+			nevents++;
+		}
+
+		nhandles++;
+
+		if (epoll_fd->next == epfd->fds) {
+			break;
+		}
+	}
+
+	return nevents;
+}
+
+static inline void epoll_destroy (epoll_t epfd) {
+	free (epfd);
+}
 
 #endif
